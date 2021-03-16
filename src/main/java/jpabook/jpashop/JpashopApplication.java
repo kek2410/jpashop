a511d45b5d671597a6521faa9f71f6d109b87cf9
kek2410 <kek2410@naver.com>
2021-03-16 10:18:44 +0900

    second commit

--- src/main/java/jpabook/jpashop/JpashopApplication.java
+++ src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JpashopApplication {
 
     public static void main(String[] args) {
+        System.out.println(111);
         SpringApplication.run(JpashopApplication.class, args);
     }
 
--- src/test/java/jpabook/jpashop/JpashopApplicationTests.java
+++ src/test/java/jpabook/jpashop/JpashopApplicationTests.java
@@ -1,12 +1,12 @@
 package jpabook.jpashop;
 
-import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class JpashopApplicationTests {
 
-    @Test
+//    @Test
     void contextLoads() {
     }
 
